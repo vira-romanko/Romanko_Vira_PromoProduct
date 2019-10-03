db6365a557791dcbe9c9e16db9d5dc0553049031
Vira <vira.romanko99@gmail.com>
2019-10-03 12:08:30 -0400

    Inital commit

---
+++ confing.js
@@ -0,0 +1,9 @@
+const config = {
+    uname: "root",
+    upass: "root",
+    port: 8889,
+    host: 'localhost',
+    db:"bizcard"
+}
+// make this a public file our app can read
+module.exports = config;
